eec5f50e40f8a1e3040baf93b704449f8fed30de
Tim Perry <tim@forepoint.co.uk>
2013-09-02 20:14:41 +0100

    Fixes to pid system

--- run_backup.rb
+++ run_backup.rb
@@ -4,24 +4,38 @@ abort( "Bakman only runs on ruby 1.9+, please update Ruby" ) unless RUBY_VERSION
 
 # abort unless all the dependencies are installed
 begin
+
   require 'rubygems'
   require_relative 'lib/pidfile.class.rb'
   require_relative 'core/Bakman.class.rb'
   gem 'net-ssh'
   gem 'net-scp'
   gem 'activesupport'
+
 rescue Exception => e
+
   abort( "Dependencies not installed please run the install script.\nSimply run ./INSTALL" )
+
 end
 
 begin
 
-  # run the backup whilst using the pid file
+  # kill backups if still running
+  if File.exists?( "./bakman.pid" )  
+
+    Process.kill( 15, File.read( './bakman.pid' ).to_i ) 
+    File.delete( "./bakman.pid" );
+
+  end
+
+  # grab a pidfile
   PidFile.new( :piddir => File.dirname( __FILE__ ), :pidfile => "bakman.pid" )
+
+  # run the backup whilst using the pid file
   Bakman.run
 
 rescue Exception => e
-  
+
   puts "#{e}"
-  
+
 end
